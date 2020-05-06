54bc35a45946d92ce76910981d05481558c0c4f7
rjdoubleu <waldenr1@gmail.com>
2020-05-06 20:43:51 +0000

    Callbacks update

--- ResNet Model/callbacks.py
+++ ResNet Model/callbacks.py
@@ -1,8 +1,36 @@
 import wandb
 import tensorflow as tf
 
-class WandbBatchHistory(tf.keras.callbacks.Callback,):
+# Custom callback that performs batchwise updates
+
+class WandbBatchHistory(tf.keras.callbacks.Callback):
+    
+    # track the steps and epochs for unique step values
+    train_epoch_iter = 0
+    train_step_iter = 0
+    val_epoch_iter = 0
+    val_step_iter = 0
+    
+    # log training batch metrics
+    def on_batch_end(self, batch, logs=None):
+        step = batch + self.train_epoch_iter
+        wandb.log({'loss':logs['loss']}, step=step, commit=False)
+        wandb.log({'accuracy':logs['accuracy']}, step=step)
+        self.train_step_iter += 1
         
-    def on_train_batch_end(self, batch, logs=None):
-        wandb.log({'loss':logs['loss']}, step=batch, commit=False)
-        wandb.log({'accuracy':logs['accuracy']}, step=batch)
+    # log validation batch metrics
+    def on_test_batch_end(self, batch, logs=None):
+        step = batch + self.val_epoch_iter
+        wandb.log({'val_loss':logs['loss']}, step=step, commit=False)
+        wandb.log({'val_accuracy':logs['accuracy']}, step=step)
+        self.val_step_iter += 1
+    
+    # log training epoch final iteration
+    def on_epoch_end(self, epoch, logs=None):
+        self.train_epoch_iter = self.train_step_iter
+    
+    # log validation epoch final iteration
+    def on_test_epoch_end(self, epoch, logs=None):
+        self.val_epoch_iter = self.val_step_iter
+        
+        
